b0128cfdebe5915b7af87781d817151910139e14
Elise Hein <elise@moo.com>
2016-08-02 14:46:44 +0100

    Instructions screen navigationbar styling

--- Numbers/Instructions.swift
+++ Numbers/Instructions.swift
@@ -13,13 +13,35 @@ class Instructions: UIViewController {
 
     init() {
         super.init(nibName: nil, bundle: nil)
-        title = "Instructions"
-        view.backgroundColor = UIColor.grayColor()
+        title = "How to play"
+        view.backgroundColor = UIColor.themeColor(.OffWhite)
     }
 
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
+
         navigationController?.setNavigationBarHidden(false, animated: true)
+
+
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: .Default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.translucent = true
+
+        navigationController?.navigationBar.tintColor = UIColor.themeColor(.OffBlack)
+        let navigationTitleFont = UIFont.themeFontWithSize(14, weight: .Bold    )
+        navigationController?.navigationBar.titleTextAttributes = [NSFontAttributeName: navigationTitleFont]
+
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 20, height: 16))
+        button.setBackgroundImage(UIImage(named: "back-arrow"), forState: .Normal)
+        button.addTarget(self,
+                         action: #selector(Instructions.goBack),
+                         forControlEvents: .TouchUpInside)
+        let backButton = UIBarButtonItem(customView: button)
+        navigationItem.leftBarButtonItem = backButton
+    }
+
+    func goBack() {
+        navigationController?.popViewControllerAnimated(true)
     }
 
     required init?(coder aDecoder: NSCoder) {
